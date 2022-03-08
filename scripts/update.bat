b880f732fe23ac0fb3f006513d8e791f897b8017
Marcus Yong <marcusyqy@gmail.com>
2022-03-08 21:48:17 +0800

    added update.bat for unsufficient priviledges pc

--- scripts/update.bat
+++ scripts/update.bat
@@ -2,7 +2,6 @@
 pushd %~dp0\..\
 echo "deleting compile_commands.json"
 del /F /Q compile_commands.json
-echo "deleted compile_commands.json"
 echo "linking compile_commands.json to %1 build"
-call cmd /c mklink compile_commands.json .\compile_commands\%1.json
+call xcopy /s .\compile_commands\%1.json compile_commands.json*
 popd
